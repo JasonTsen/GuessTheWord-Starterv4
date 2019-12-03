18ea1fecc634da95f12432cefd350ea3482d0a2c
JasonTsen <sjtsen98"gmail.com>
2019-12-03 14:03:42 +0800

    half

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,16 +1,26 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
     // The current word
-    var word = ""
+    private val _word = MutableLiveData<String>()
+    val word: LiveData<String>
+        get() = _word
 
     // The current score
-    var score = 0
-
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
     // The list of words - the front of the list is the next word to guess
+
+    private val _eventGameFinish = MutableLiveData<Boolean>()
+    val eventGameFinish: LiveData<Boolean>
+        get() = _eventGameFinish
+
     private lateinit var wordList: MutableList<String>
     private fun resetList() {
         wordList = mutableListOf(
@@ -39,37 +49,51 @@ class GameViewModel : ViewModel() {
         wordList.shuffle()
     }
     init {
+        _word.value = ""
+        _score.value = 0
         resetList()
         nextWord()
 
         Log.i("GameViewModel", "GameViewModel created!")
     }
-
+    /** Method for the game completed event **/
+    fun onGameFinish() {
+        _eventGameFinish.value = true
+    }
         private fun nextWord() {
-            if (!wordList.isEmpty()) {
+            if (wordList.isEmpty()) {
+
+                onGameFinish()
+            }
+            else{
                 //Select and remove a word from the list
-                word = wordList.removeAt(0)
+                _word.value = wordList.removeAt(0)
             }
 
-        }
+
+
+            }
+
+
         /** Methods for buttons presses **/
+        override fun onCleared() {
+            super.onCleared()
+            Log.i("GameViewModel", "GameViewModel destroyed!")
+        }
 
         fun onSkip() {
             if (!wordList.isEmpty()) {
-                score--
+                _score.value = (score.value)?.minus(1)
             }
             nextWord()
         }
 
         fun onCorrect() {
             if (!wordList.isEmpty()) {
-                score++
+                _score.value = (score.value)?.plus(1)
             }
             nextWord()
         }
 
-    override fun onCleared() {
-        super.onCleared()
-        Log.i("GameViewModel", "GameViewModel destroyed!")
-    }
+
 }
