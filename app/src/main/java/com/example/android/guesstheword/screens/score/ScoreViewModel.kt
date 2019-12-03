fae826814d280b4778a3c78d059b223b3edb0dad
JasonTsen <sjtsen98@gmail.com>
2019-12-03 23:10:02 +0800

    code challenge timeformat not yet

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
@@ -1,8 +1,10 @@
 package com.example.android.guesstheword.screens.score
 
+import android.text.format.DateUtils
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class ScoreViewModel (finalScore: Int) : ViewModel() {
@@ -12,11 +14,24 @@ class ScoreViewModel (finalScore: Int) : ViewModel() {
     val eventPlayAgain: LiveData<Boolean>
         get() = _eventPlayAgain
 
+    // Countdown time
+    private val _currentTime = MutableLiveData<Long>()
+    val currentTime: LiveData<Long>
+        get() = _currentTime
+
+    val newResult = Transformations.map(currentTime) { time ->
+        // Do some transformation on the input live data
+        "Current time is :" + DateUtils.formatElapsedTime(time).length
+        // and return the new value
+
+    }
+
     val score: LiveData<Int>
         get() = _score
     init {
         Log.i("ScoreViewModel", "Final score is $finalScore")
         _score.value = finalScore
+
     }
     fun onPlayAgain() {
         _eventPlayAgain.value = true
